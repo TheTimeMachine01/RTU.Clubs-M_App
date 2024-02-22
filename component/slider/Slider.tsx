8286035eb2f78b4b49e747053a20d8954103e8ac
nitinco <106646986+nitinco@users.noreply.github.com>
2024-02-22 23:03:58 +0530

    slider is complete

--- component/slider/Slider.tsx
+++ component/slider/Slider.tsx
@@ -1,32 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
+// Slider.js
+
+import { Dimensions, StyleSheet, Text, View, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
+import React from 'react';
+import sliderData from './sliderData';
+
+const { height, width } = Dimensions.get('window');
 
 const Slider = () => {
   return (
-    <>
-    <View  style={[styles.slider,tw`m-5 rounded-8`]}>
-      <Text>Slider</Text>
+    <View style={styles.slider}>
+      <FlatList
+        horizontal
+        pagingEnabled
+        data={sliderData}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.sliderM}>
+              <TouchableOpacity style={styles.button} disabled={true}>
+                <ImageBackground source={item.imageUrl} style={styles.imageBackground}>
+                  <Text style={styles.text}>{item.text}</Text>
+                </ImageBackground>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
     </View>
-    <View style={styles.slidertexts}>
-      <Text>club-name</Text>
-    </View>
-    </>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
 
 const styles = StyleSheet.create({
-  slider:{
-    justifyContent:'center',
-    alignItems:'center',
-    height:400,
-    backgroundColor:'red'
+  slider: {
+    width: width,
+    height: height,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'red'
   },
-  slidertexts:{
-    justifyContent:'center',
-    alignItems:'center',
-
-  }
-})
+  sliderM: {
+    width: width,
+    height: height / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: '90%',
+    height: '90%',
+    borderRadius: 20,
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: '#0afa4e',
+    position:'absolute',
+    bottom:10,
+    right:10,
+    textAlign: 'center',
+    backgroundColor:'transparent',
+    borderColor:'#0afa4e',
+    borderWidth:1,
+    borderRadius:5, // Semi-transparent background for better readability
+    padding: 10,
+  },
+});
---
+++ component/slider/sliderData.tsx
@@ -0,0 +1,19 @@
+// sliderData.tsx
+
+const sliderData = [
+    {
+        imageUrl: require('./images/nitin.jpg'),
+        text: 'Join Now'
+    },
+    {
+        imageUrl: require('./images/ashish.jpg'),
+        text: 'Join Now'
+    },
+    {
+        imageUrl: require('./images/sadhaya.jpg'),
+        text: 'Join Now'
+    },
+    // Add more items as needed
+];
+
+export default sliderData;
